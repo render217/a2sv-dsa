613359281df5d311b2ac02df3e9f1fd24b0910b9
Beamlak <142716427+render217@users.noreply.github.com>
2026-01-24 17:26:06 +0300

    arthmetic_operators.py

---
+++ arthmetic_operators.py
@@ -0,0 +1,10 @@
+# problem https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+# submission https://www.hackerrank.com/challenges/python-arithmetic-operators/submissions/code/460823874
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    

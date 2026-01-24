62b717af0c0c3ef0e3918c0dded2a8c407ad33ed
Beamlak <142716427+render217@users.noreply.github.com>
2026-01-24 17:30:16 +0300

    division.py

---
+++ division.py
@@ -0,0 +1,9 @@
+# Problem:  https://www.hackerrank.com/challenges/python-division/problem
+# Submission: https://www.hackerrank.com/challenges/python-division/submissions/code/460824289
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print( a // b)
+    print(a / b)
+    

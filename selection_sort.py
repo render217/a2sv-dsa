231c96fe34df6c906c2faee4b50796f95c45d040
Beamlak S. <142716427+render217@users.noreply.github.com>
2026-03-08 22:28:09 +0300

    selection_sort

---
+++ selection_sort.py
@@ -0,0 +1,9 @@
+class Solution: 
+    def selectionSort(self, arr):
+        for i in range(len(arr)):
+            min_idx = i
+            for j in range(i+1,len(arr)):
+                if arr[j] < arr[min_idx]:
+                    min_idx = j
+            arr[min_idx],arr[i] = arr[i],arr[min_idx]
+        return arr

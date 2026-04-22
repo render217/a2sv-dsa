0b75b5c9cfc2af2eea6d65cb94a824e862a97ed4
Beamlak S. <142716427+render217@users.noreply.github.com>
2026-04-22 22:22:22 +0300

    Add solution for Clear Digits (trial 1)

---
+++ leetcode/clear-digits_trial1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if char.isdigit() and stack:
+                stack.pop()
+            else:
+                stack.append(char)
+        return "".join(stack)
